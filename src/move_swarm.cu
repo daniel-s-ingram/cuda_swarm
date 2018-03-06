06c65ae5384bbed6c47b79f60ac0db7d42d34010
Daniel Ingram <ingramds@appstate.edu>
2018-03-06 13:33:59 -0500

    Comparing CPU and GPU implementation times - GPU has a slight advantage, will probably improve with more agents

--- src/move_swarm.cu
+++ src/move_swarm.cu
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <boost/lexical_cast.hpp>
 #include <math.h>
+#include <time.h>
 
 #include "ros/ros.h"
 #include "gazebo_msgs/ModelState.h"
@@ -11,7 +12,7 @@
 
 __global__ void trajectories(gazebo_msgs::ModelState *msg, float x, float y, float z, float w, float t)
 {
-	int i = threadIdx.x;
+	int i = threadIdx.x + blockIdx.x * blockDim.x;
 
 	msg[i].pose.position.x = sin(t) + i/10;
 	msg[i].pose.position.y = sin(t) + i%10;
@@ -36,29 +37,55 @@ int main(int argc, char **argv)
 		swarm_msg[i].model_name = "quadrotor" + boost::lexical_cast<std::string>(i);
 	}
 
-	long size = 100*sizeof(gazebo_msgs::ModelState);
-	cudaError_t error;
+	long msg_size = 100*sizeof(gazebo_msgs::ModelState);
 	gazebo_msgs::ModelState *d_msg;
-	if ((error = cudaMalloc((void **)&d_msg, size)) != cudaSuccess)
+
+	cudaError_t error;
+	if ((error = cudaMalloc((void **)&d_msg, msg_size)) != cudaSuccess)
 	{
 		printf("Error allocating d_a: %s in %s on line %d\n", cudaGetErrorString(error), __FILE__, __LINE__);
 		exit(EXIT_FAILURE);
 	}
 
-	if ((error = cudaMemcpy(d_msg, swarm_msg, size, cudaMemcpyHostToDevice)) != cudaSuccess)
+	if ((error = cudaMemcpy(d_msg, swarm_msg, msg_size, cudaMemcpyHostToDevice)) != cudaSuccess)
 	{
 		printf("Error copying a to d_a: %s in %s on line %d\n", cudaGetErrorString(error), __FILE__, __LINE__);
 		exit(EXIT_FAILURE);
 	}
 
+	clock_t initial, final;
 	float t = 0;
 	tf::Quaternion q;
 	while (ros::ok())
 	{
 		q = tf::createQuaternionFromRPY(-0.5*cos(t), 0.5*cos(t), 0);
-		trajectories<<<1,100>>>(d_msg, q[0], q[1], q[2], q[3], t);
-		cudaMemcpy(swarm_msg, d_msg, size, cudaMemcpyDeviceToHost);
+
+		initial = clock();
+		trajectories<<<100,512>>>(d_msg, q[0], q[1], q[2], q[3], t);
+		cudaMemcpy(swarm_msg, d_msg, msg_size, cudaMemcpyDeviceToHost);
 		for (int i = 0; i < 100; i++) swarm_pub.publish(swarm_msg[i]);
+		final = clock();
+
+		std::cout << "Time taken by GPU: " << (double)(final-initial)/CLOCKS_PER_SEC << std::endl;
+
+		initial = clock();
+		for (int i = 0; i < 100; i++)
+		{
+			swarm_msg[i].pose.position.x = sin(t) + i/10;
+			swarm_msg[i].pose.position.y = sin(t) + i%10;
+			swarm_msg[i].pose.position.z = sin(t+i) + 2;
+
+			swarm_msg[i].pose.orientation.x = q[0];
+			swarm_msg[i].pose.orientation.y = q[1];
+			swarm_msg[i].pose.orientation.z = q[2];
+			swarm_msg[i].pose.orientation.w = q[3];
+
+			swarm_pub.publish(swarm_msg[i]);
+		}
+		final = clock();
+
+		std::cout << "Time taken by CPU: " << (double)(final-initial)/CLOCKS_PER_SEC << std::endl;
+		
 		t += 0.001;
 	}
 
