527af9c9dfe6cd9a17c5975a9bd9d3fa0c13fb9e
Daniel Ingram <ingramds@appstate.edu>
2018-03-06 13:34:40 -0500

    Removed old lines

--- bash/swarm_launch.sh
+++ bash/swarm_launch.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-#roslaunch cuda_swarm swarm0-24.launch
-#roslaunch cuda_swarm swarm25-49.launch
-#roslaunch cuda_swarm swarm50-74.launch
-#roslaunch cuda_swarm swarm75-99.launch
-
 for f in ../launch/swarms/*.launch
 do
 	roslaunch $f
