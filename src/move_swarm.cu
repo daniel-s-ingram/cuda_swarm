7cc1d1e42c71aad81d53c1af21ba67155e491a06
Daniel Ingram <ingramds@appstate.edu>
2018-03-05 22:37:18 -0500

    CUDA implementation - slower than expected, needs work

--- src/move_swarm.cu
+++ src/move_swarm.cu
@@ -9,9 +9,18 @@
 
 #include "cuda.h"
 
-__global__ void trajectories(gazebo_msgs::ModelState *msg, tf::Quaternion *quat, float t)
+__global__ void trajectories(gazebo_msgs::ModelState *msg, float x, float y, float z, float w, float t)
 {
+	int i = threadIdx.x;
 
+	msg[i].pose.position.x = sin(t) + i/10;
+	msg[i].pose.position.y = sin(t) + i%10;
+	msg[i].pose.position.z = sin(t+i) + 2;
+
+	msg[i].pose.orientation.x = x;
+	msg[i].pose.orientation.y = y;
+	msg[i].pose.orientation.z = z;
+	msg[i].pose.orientation.w = w;
 }
 
 int main(int argc, char **argv)
@@ -19,51 +28,40 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "move_swarm");
 	ros::NodeHandle node;
 
-	ros::Publisher *swarm_pub = new ros::Publisher[100];
+	ros::Publisher swarm_pub = node.advertise<gazebo_msgs::ModelState>("/gazebo/set_model_state", 100);
 	gazebo_msgs::ModelState *swarm_msg = new gazebo_msgs::ModelState[100];
 
 	for (int i = 0; i < 100; i++)
 	{
-		swarm_pub[i] = node.advertise<gazebo_msgs::ModelState>("/gazebo/set_model_state", 1);
 		swarm_msg[i].model_name = "quadrotor" + boost::lexical_cast<std::string>(i);
 	}
 
+	long size = 100*sizeof(gazebo_msgs::ModelState);
 	cudaError_t error;
 	gazebo_msgs::ModelState *d_msg;
-	if ((error = cudaMalloc((void **)&d_msg, 100*sizeof(gazebo_msgs::ModelState))) != cudaSuccess)
+	if ((error = cudaMalloc((void **)&d_msg, size)) != cudaSuccess)
 	{
 		printf("Error allocating d_a: %s in %s on line %d\n", cudaGetErrorString(error), __FILE__, __LINE__);
 		exit(EXIT_FAILURE);
 	}
 
-	if ((error = cudaMemcpy(d_msg, swarm_msg, 100*sizeof(gazebo_msgs::ModelState), cudaMemcpyHostToDevice)) != cudaSuccess)
+	if ((error = cudaMemcpy(d_msg, swarm_msg, size, cudaMemcpyHostToDevice)) != cudaSuccess)
 	{
 		printf("Error copying a to d_a: %s in %s on line %d\n", cudaGetErrorString(error), __FILE__, __LINE__);
 		exit(EXIT_FAILURE);
 	}
 
 	float t = 0;
-	tf::Quaternion quaternion;
+	tf::Quaternion q;
 	while (ros::ok())
 	{
-		quaternion = tf::createQuaternionFromRPY(-0.5*cos(t), 0.5*cos(t), 0);
-		
-		for (int i = 0; i < 100; i++)
-		{
-			swarm_msg[i].pose.position.x = sin(t) + i/10;
-			swarm_msg[i].pose.position.y = sin(t) + i%10;
-			swarm_msg[i].pose.position.z = sin(t+i) + 2;
-
-			swarm_msg[i].pose.orientation.x = quaternion[0];
-			swarm_msg[i].pose.orientation.y = quaternion[1];
-			swarm_msg[i].pose.orientation.z = quaternion[2];
-			swarm_msg[i].pose.orientation.w = quaternion[3];
-
-			swarm_pub[i].publish(swarm_msg[i]);
-		}
+		q = tf::createQuaternionFromRPY(-0.5*cos(t), 0.5*cos(t), 0);
+		trajectories<<<1,100>>>(d_msg, q[0], q[1], q[2], q[3], t);
+		cudaMemcpy(swarm_msg, d_msg, size, cudaMemcpyDeviceToHost);
+		for (int i = 0; i < 100; i++) swarm_pub.publish(swarm_msg[i]);
 		t += 0.001;
 	}
 
-	delete[] swarm_pub; 
 	delete[] swarm_msg;
+	cudaFree(d_msg);
 }
